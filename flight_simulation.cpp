ddec5ac027e400256d7ff5f75f2c532b2d1390e6
Abhishek Sinha <curiousindian16@gmail.com>
2025-08-25 10:30:31 +0530

    Fix thread-safety issues in client-server seat reservation simulation

--- flight_simulation.cpp
+++ flight_simulation.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
-#include <time.h>
 #include <queue>
 #include <set>
 #include <random>
@@ -9,130 +8,147 @@
 
 using namespace std;
 
-// This struct provides information sharing between client and server thread
-struct Pipe {
-    queue<int> clientToServerMessages;
-    queue<bool> serverToClientMessages;
-};
-
-// This struct is for storing information of each Client
-struct Client {
-    int sleepTime;
-    int clientId;
-    int reservedSeatNo;
-    int pipeId;
+// Struct for a reservation request
+struct Request { 
+    int clientId; // ID of client making request
+    int seatNo;   // Seat to reserve
 };
 
 int clientSize;
 ofstream outputTextFile;
-set<int> availableSeats;
-vector<Pipe> pipes;
-vector<Client> clients;
-vector<HANDLE> semClient;  // Semaphores for client signaling
-vector<HANDLE> semServer;  // Semaphores for server signaling
-CRITICAL_SECTION reservationMutex;
-CRITICAL_SECTION writeMutex;
+set<int> availableSeats;        // Tracks free seats
+vector<HANDLE> semClient;       // Each client signals server
+HANDLE semServer;               // Server signals back to client
+CRITICAL_SECTION reservationMutex, writeMutex, queueMutex;
+queue<Request> requestQueue;    // Queue of requests from clients
 
+struct Client { 
+    int clientId; 
+    int sleepTime;          // Wait before making request (ms)
+    int reservedSeatNo;     // Assigned seat number
+};
+
+vector<Client> clients;
+
+// Client thread function
 DWORD WINAPI ClientFun(LPVOID data) {
     Client* client = (Client*)data;
-    Sleep(client->sleepTime / 1000);  // Convert microseconds to milliseconds
-    Pipe* p = &pipes[client->pipeId];
-    srand((unsigned int)time(NULL));
-    
+    Sleep(client->sleepTime); // simulate client thinking
+
+    int seatNo = -1;
+
+    // Pick a seat safely
     EnterCriticalSection(&reservationMutex);
-    int size = availableSeats.size();
-    set<int>::iterator it = availableSeats.begin();
-    advance(it, rand() % size);
-    int seatNo = *it;
-    availableSeats.erase(it);
-    LeaveCriticalSection(&reservationMutex);
-    
-    p->clientToServerMessages.push(seatNo);
-    ReleaseSemaphore(semClient[client->clientId - 1], 1, NULL);
-    WaitForSingleObject(semServer[client->clientId - 1], INFINITE);
-    
-    if (p->serverToClientMessages.front()) {
-        p->serverToClientMessages.pop();
+    if (!availableSeats.empty()) {
+        auto it = availableSeats.begin();
+        advance(it, rand() % availableSeats.size());
+        seatNo = *it;
+        availableSeats.erase(it);
     }
+    LeaveCriticalSection(&reservationMutex);
+
+    if (seatNo != -1) {
+        // Push request to queue safely
+        EnterCriticalSection(&queueMutex);
+        requestQueue.push({client->clientId, seatNo});
+        LeaveCriticalSection(&queueMutex);
+
+        ReleaseSemaphore(semClient[client->clientId - 1], 1, NULL); // notify server
+        WaitForSingleObject(semServer, INFINITE);                   // wait for server
+
+        client->reservedSeatNo = seatNo; // store seat locally
+    }
+
     return 0;
 }
 
+// Single server thread
 DWORD WINAPI ServerFun(LPVOID data) {
-    Client* client = (Client*)data;
-    Pipe* p = &pipes[client->pipeId];
-    
-    WaitForSingleObject(semClient[client->clientId - 1], INFINITE);
-    int seatNo = p->clientToServerMessages.front();
-    p->clientToServerMessages.pop();
-    client->reservedSeatNo = seatNo;
-    
-    EnterCriticalSection(&writeMutex);
-    outputTextFile << "Client" << client->clientId << " reserves Seat" << client->reservedSeatNo << endl;
-    LeaveCriticalSection(&writeMutex);
-    
-    p->serverToClientMessages.push(true);
-    ReleaseSemaphore(semServer[client->clientId - 1], 1, NULL);
+    int processed = 0;
+
+    while (processed < clientSize) {
+        for (int i = 0; i < clientSize; i++) {
+            WaitForSingleObject(semClient[i], 0); // check if client signaled
+
+            EnterCriticalSection(&queueMutex);
+            while (!requestQueue.empty()) {
+                Request req = requestQueue.front();
+                requestQueue.pop();
+
+                EnterCriticalSection(&writeMutex);
+                outputTextFile << "Client" << req.clientId
+                               << " reserves Seat" << req.seatNo << endl;
+                LeaveCriticalSection(&writeMutex);
+
+                processed++;
+                ReleaseSemaphore(semServer, 1, NULL); // notify client
+            }
+            LeaveCriticalSection(&queueMutex);
+        }
+    }
     return 0;
 }
 
 int main(int argc, char* argv[]) {
-    if (argc < 2) {
-        cout << "You need to give an argument" << endl;
-        cout << "e.g. flight_simulation.exe [seat_number]" << endl;
-        return 0;
+    if (argc < 2) { 
+        cout << "Usage: flight_simulation.exe [seat_number]\n"; 
+        return 0; 
     }
-    
+
     clientSize = atoi(argv[1]);
+
+    // Initialize critical sections for thread safety
     InitializeCriticalSection(&reservationMutex);
     InitializeCriticalSection(&writeMutex);
+    InitializeCriticalSection(&queueMutex);
 
-    // Initialize semaphores
-    for (int i = 0; i < clientSize; i++) {
-        semClient.push_back(CreateSemaphore(NULL, 0, 1, NULL));
-        semServer.push_back(CreateSemaphore(NULL, 0, 1, NULL));
-    }
-    
+    semServer = CreateSemaphore(NULL, 0, LONG_MAX, NULL);
+
+    // Each client gets a semaphore to signal server
+    for (int i = 0; i < clientSize; i++)
+        semClient.push_back(CreateSemaphore(NULL, 0, LONG_MAX, NULL));
+
+    // Open file to write reservation logs
     outputTextFile.open("output.txt");
     outputTextFile << "Number of total seats: " << clientSize << endl;
-    
+
+    // Initialize available seats
+    for (int i = 1; i <= clientSize; i++)
+        availableSeats.insert(i);
+
+    srand((unsigned int)time(NULL)); // seed RNG
+
+    // Setup clients
+    clients.resize(clientSize);
     for (int i = 0; i < clientSize; i++) {
-        availableSeats.insert(i + 1);
+        clients[i].clientId = i + 1;
+        clients[i].reservedSeatNo = -1;
+        clients[i].sleepTime = rand() % 151 + 50; // 50-200 ms
     }
-    
+
+    // Start client threads
     vector<HANDLE> clientThreads(clientSize);
-    vector<HANDLE> serverThreads(clientSize);
-    srand((unsigned int)time(NULL));
-    
-    for (int i = 0; i < clientSize; i++) {
-        Client data;
-        data.clientId = i + 1;
-        data.sleepTime = (rand() % 151 + 50) * 1000;
-        data.reservedSeatNo = -1;
-        data.pipeId = i;
-        clients.push_back(data);
-        Pipe pipe;
-        pipes.push_back(pipe);
-    }
-    
-    for (int i = 0; i < clientSize; i++) {
+    for (int i = 0; i < clientSize; i++)
         clientThreads[i] = CreateThread(NULL, 0, ClientFun, &clients[i], 0, NULL);
-        serverThreads[i] = CreateThread(NULL, 0, ServerFun, &clients[i], 0, NULL);
-    }
-    
-    WaitForMultipleObjects(clientSize, serverThreads.data(), TRUE, INFINITE);
-    
+
+    // Start single server thread
+    HANDLE serverThread = CreateThread(NULL, 0, ServerFun, NULL, 0, NULL);
+
+    // Wait for clients and server to finish
+    WaitForMultipleObjects(clientSize, clientThreads.data(), TRUE, INFINITE);
+    WaitForSingleObject(serverThread, INFINITE);
+
     outputTextFile << "All seats are reserved." << endl;
     outputTextFile.close();
-    
+
     // Cleanup
     DeleteCriticalSection(&reservationMutex);
     DeleteCriticalSection(&writeMutex);
-    for (int i = 0; i < clientSize; i++) {
-        CloseHandle(clientThreads[i]);
-        CloseHandle(serverThreads[i]);
-        CloseHandle(semClient[i]);
-        CloseHandle(semServer[i]);
-    }
-    
+    DeleteCriticalSection(&queueMutex);
+    CloseHandle(semServer);
+    for (int i = 0; i < clientSize; i++) CloseHandle(semClient[i]);
+    for (int i = 0; i < clientSize; i++) CloseHandle(clientThreads[i]);
+    CloseHandle(serverThread);
+
     return 0;
 }
